4f3d0568fedb58658d2a1205b7a5ffef815fb138
mullenlucas <mullenlucas@gmail.com>
2023-01-20 12:18:05 -0300

    create react and update views and config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h2>Hello World!</h2>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
